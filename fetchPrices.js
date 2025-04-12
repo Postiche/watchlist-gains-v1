97df8ef4172c3f02f4e5bc649c2d9996368fa30e
Postiche <cochet.guillaume@gmail.com>
2025-04-13 00:10:27 +0200

    Add files via upload

--- fetchPrices.js
+++ fetchPrices.js
@@ -1,7 +1,1 @@
-// utils/fetchPrices.js
-export async function fetchYahooPrice(symbol) {
-  const encodedSymbol = encodeURIComponent(symbol);
-  const response = await fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${encodedSymbol}?interval=1d`);
-  const data = await response.json();
-  return data.chart.result[0].meta.regularMarketPrice;
-}
+// Fonctions pour appeler les API financières en temps réel
